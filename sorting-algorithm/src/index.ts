3de00020af968dd53f0f2cb73e88fe0bd4476ab8
farid ghaderi <farid.51gh@gmail.com>
2021-10-29 21:02:54 +0330

    finish project 2

--- sorting-algorithm/src/index.ts
+++ sorting-algorithm/src/index.ts
@@ -1,2 +1,27 @@
-console.log("hey");
-console.log("he2");
+import { CharactersColletion } from "./interfaces/CharactersCollection";
+import { NumbersCollection } from "./interfaces/NumbersColletion";
+import { LinkedList } from "./interfaces/LinkedList";
+
+const charactersCollection = new CharactersColletion("fari");
+console.log(charactersCollection);
+
+charactersCollection.sort();
+
+console.log(charactersCollection);
+
+const numbersCollection = new NumbersCollection([1, 5, -5]);
+
+numbersCollection.sort();
+
+console.log(numbersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(14);
+linkedList.add(5);
+linkedList.add(8);
+linkedList.add(-1);
+linkedList.add(-2);
+
+linkedList.sort();
+
+console.log(linkedList);
